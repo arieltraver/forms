6a1098d28d8a444b822af8c70e426a529f3d3a73
Ariel Traver <at5@cs.wellesley.edu>
2021-10-29 14:42:01 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import (Flask, render_template)
+app = Flask(__name__)
+
+@app.route('/')
+def campus():
+    return render_template('campus.html')
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0',port)
